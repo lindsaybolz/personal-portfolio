28a3ec69c22a817ceeabd380d3b02bd02790cc40
Lindsay <lindsaybolzwilson@gmail.com>
2023-09-09 16:00:02 -0600

    Updated map key, footer.

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import USAMap from "react-usa-map";
 import './Map.css';
 import stateData from '../resources/stateData.json';
-import { HashScroll } from "react-hash-scroll";
 
 const Map = () => { 
     const [description, setDescription] = useState("Click around to see what I've been up to and where!")
@@ -10,7 +9,7 @@ const Map = () => {
 
     // Mandatory
     const mapHandler = (event) => {
-        console.log(event.target.dataset.name)
+        // console.log(event.target.dataset.name)
         let fullName = stateData[event.target.dataset.name]['fullName']
         let message = stateData[event.target.dataset.name]['message']
         setDescription(`${fullName}: ${message}`)
@@ -22,12 +21,15 @@ const Map = () => {
                         className='statePhoto'
                         src={require(`${photoPath}`)}
                         alt={`Lindsay in ${stateData[event.target.dataset.name]['fullName']}`}
-                        height={200}
                         />
                 </div>
             );
         } else {
             setImage("")
+        }
+        const adventuresElement = document.getElementById('adventures');
+        if (adventuresElement) {
+            adventuresElement.scrollIntoView({ behavior: 'smooth' });
         }
     };
 
@@ -37,33 +39,33 @@ const Map = () => {
 
 
     return (
-        // <HashScroll hash='#adventures' >
         <div className="mapContainer" id='adventures'>
-        <USAMap 
-            className='usaMap'
-            onClick={mapHandler} 
-            width={'80vw'}
-            height={'auto'}
-            title='Lindsays Map'
-            defaultFill='#D2A1A8'
-            customize={statesCustomConfig()}
-            />
-        <div className="mapKey">
-            <div className="bigHitters item">
-            Big Hitters
+            <USAMap 
+                className='usaMap'
+                id='usaMap'
+                onClick={mapHandler} 
+                width={'80vw'}
+                height={'auto'}
+                title='Lindsays Map'
+                defaultFill='#D2A1A8'
+                customize={statesCustomConfig()}
+                />
+            <div className="mapKey">
+                <div className="item">
+                Big Hitters &nbsp;
+                    <div className="bigHitters">.&nbsp;&nbsp;&nbsp;</div>
+
+                </div>
+                <div className="item">
+                    Something fun &nbsp;
+                    <div className="somethingFun">&nbsp;&nbsp;&nbsp;&nbsp;</div>
+                </div>
             </div>
-            <div className="somethingFun item">
-            Something fun
+            <div className="stateDetails" id="stateDetails">
+                {description}
+                {image}
             </div>
         </div>
-        <div className="mapDescription">
-            {description}
-        </div>
-        <div>
-            {image}
-        </div>
-        </div>
-        // </HashScroll>
     )
 }
 
